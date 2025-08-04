8e64807062e82c405df05c7dea149f19388aa040
ozcan <ozcanbarbaros45@gmail.com>
2025-08-04 14:33:08 +0300

    Main Activity is completed and Game Activity XML side finished

---
+++ CatchMeIfYouCan/app/src/main/java/com/ozcan/catchmeifyoucan/GameActivity.java
@@ -0,0 +1,28 @@
+package com.ozcan.catchmeifyoucan;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class GameActivity extends AppCompatActivity {
+
+    TextView textView2, textView3;
+    Readable readable;
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_game);
+
+
+
+    }
+}
---
+++ CatchMeIfYouCan/app/src/main/java/com/ozcan/catchmeifyoucan/MainActivity.java
@@ -0,0 +1,35 @@
+package com.ozcan.catchmeifyoucan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+    }
+
+    public void start(View view){
+        intent = new Intent(MainActivity.this,GameActivity.class);
+        startActivity(intent);
+    }
+}
