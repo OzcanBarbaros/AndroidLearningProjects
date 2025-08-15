ceb753f0d3e33ae79b59c290dfa61c509d9986c3
ozcan <ozcanbarbaros45@gmail.com>
2025-08-15 17:23:18 +0300

    Launchers created

--- ArtBook/app/src/main/java/com/ozcan/artbook/ArtActivity.java
+++ ArtBook/app/src/main/java/com/ozcan/artbook/ArtActivity.java
@@ -1,21 +1,39 @@
 package com.ozcan.artbook;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.ozcan.artbook.databinding.ActivityArtBinding;
 import com.ozcan.artbook.databinding.ActivityMainBinding;
 
 public class ArtActivity extends AppCompatActivity {
 
     private ActivityArtBinding binding;
-
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    ActivityResultLauncher<String> permissionLauncher;
+    Bitmap selectedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +53,72 @@ public class ArtActivity extends AppCompatActivity {
     }
 
     public void selectImage(View view) {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+             if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                 Snackbar.make(view,"Permisson Needed For Galery",Snackbar.LENGTH_INDEFINITE).setAction("Give Permisson", new View.OnClickListener() {
+                     @Override
+                     public void onClick(View v) {
+                         //requestpermisson
+
+                     }
+                 }).show();
+             } else {
+                 //requestpermisson
+
+             }
+
+
+        } else {
+            //gallery
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        }
         
+    }
+
+    private void registerLauncher() {
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode() == RESULT_OK) {
+                    Intent intentFromResult = result.getData();
+                    if (intentFromResult != null) {
+                        Uri imageData = intentFromResult.getData();
+
+                        try {
+                            if (Build.VERSION.SDK_INT >= 28) {
+                                ImageDecoder.Source source = ImageDecoder.createSource(ArtActivity.this.getContentResolver(),imageData);
+                                selectedImage = ImageDecoder.decodeBitmap(source);
+                                binding.imageView.setImageBitmap(selectedImage);
+                            } else {
+                                selectedImage = MediaStore.Images.Media.getBitmap(ArtActivity.this.getContentResolver(),imageData);
+                                binding.imageView.setImageBitmap(selectedImage);
+                            }
+
+
+                        } catch (Exception e) {
+
+                        }
+                    }
+                }
+
+            }
+        });
+
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if (result) {
+                    //permisson granted
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                } else {
+                    //permisson denied
+                    Toast.makeText(ArtActivity.this,"Permisson needed!",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
 }
