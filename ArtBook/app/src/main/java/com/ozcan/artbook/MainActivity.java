e4cad4c29acb3566a3cc33c258f61657e65c666a
ozcan <ozcanbarbaros45@gmail.com>
2025-08-21 16:51:41 +0300

    ArtAdapter created

--- ArtBook/app/src/main/java/com/ozcan/artbook/MainActivity.java
+++ ArtBook/app/src/main/java/com/ozcan/artbook/MainActivity.java
@@ -1,6 +1,9 @@
 package com.ozcan.artbook;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,12 +15,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.ozcan.artbook.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    ArrayList<Art> artArrayList;
+    ArtAdapter artAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +41,41 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        artArrayList = new ArrayList<>();
+
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        artAdapter = new ArtAdapter(artArrayList);
+        binding.recyclerView.setAdapter(artAdapter);
+
+        getData();
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void getData() {
+        try {
+            SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("Arts",MODE_PRIVATE,null);
+            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM arts", null);
+            int nameIx = cursor.getColumnIndex("artname");
+            int idIx = cursor.getColumnIndex("id");
+
+            while (cursor.moveToNext()) {
+                String name = cursor.getString(nameIx);
+                int id = cursor.getInt(idIx);
+                Art art = new Art(name, id);
+                artArrayList.add(art);
+
+            }
+
+            artAdapter.notifyDataSetChanged();
+
+            cursor.close();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
